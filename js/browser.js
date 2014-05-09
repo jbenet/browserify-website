08ccf495a30a3035b4f38c67efb8a5d711bac2d5
Juan Batiz-Benet <juan@benet.ai>
2014-05-08 17:36:45 -0700

    console example :)

--- js/browser.js
+++ js/browser.js
@@ -6,6 +6,7 @@ var examples = require('./examples');
 
 var w = module.exports = function() {
   w.bindHandlers();
+  console.log(w.welcome);
 }
 
 var log = function(s) {
@@ -81,3 +82,33 @@ w.onChangedTypeChain = function() {
   if ($('#text-2').val() && $('#text-1').val() && $('#type-chain').val())
     w.onClickTransform();
 }
+
+w.welcome = '\n\
+\n\
+Greetings! You can use transformer directly from this javascript console.\n\
+There is a `transformer` variable ready to go! Try it out, copy the code \n\
+below and paste it into the console:\n\
+-------------------------------------------------------------------------\n\
+\n\
+// set up a transformer function to convert base64 to ascii\n\
+var base64ToAscii = transformer("base64", "ascii");\n\
+\n\
+// a secret message!\n\
+var secret = "Q29vbCEgWW91IGp1c3QgY3JlYXRlZCBhIHRyYW5zZm9ybWVyIGZ1bmN0aW9u\
+IChgYmFzZTY0VG9Bc2NpaWApIGFuZCBjb252ZXJ0ZWQgdGhlIGJsb2NrIG9mIGhleCBpbnRvIH\
+RoaXMgbWVzc2FnZS4gV2Fzbid0IHRoYXQgZWFzeT8gV2l0aCB0cmFuc2Zvcm1lciwgd2Ugd2Fu\
+dCB0byBtYWtlIF9hbGxfIGNvbnZlcnNpb25zIHRoYXQgZWFzeSwgd2hldGhlciB5b3UncmUgY2\
+9udmVydGluZyB0aW1lc3RhbXBzLCBjeXBoZXJ0ZXh0cywgb3IgZXZlbiBpbWFnZXMuIENvbnZl\
+cnQgYWxsIHRoZSB0aGluZ3MhCgpEb2N1bWVudGF0aW9uIGlzIG5vdCBhbGwgdGhlcmUgeWV0LC\
+BidXQgY2hlY2sgb3V0IHRoZSBnaXRodWIgcmVwbyAoaHR0cDovL2dpdGh1Yi5jb20vamJlbmV0\
+L3RyYW5zZm9ybWVyKSBhbmQgdGhlICJKYXZhc2NyaXB0IiBleGFtcGxlcyBvbiB0aGlzIHdlYn\
+NpdGUuIChMZXQgdXMga25vdyBpZiBzb21lIGNvbnZlcnNpb25zIGRvbid0IHdvcmshKSBJZiB5\
+b3UgZ2V0IHN0dWNrLCBwaW5nIHVzIG9uIGlyYywgdHdpdHRlciwgb3IgZ2l0aHViISA6KQ==";\n\
+\n\
+// convert!\n\
+var message = base64ToAscii(secret);\n\
+\n\
+// display the message\n\
+console.log("The secret message is: " + message);\n\
+\n\
+\n';
